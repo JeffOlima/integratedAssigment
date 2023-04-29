81f3426aced5228195f3269e7d2c6f6d1fb32a37
Jefferson Oliveira Lima <oliveiralima.jefferson@gmail.com>
2023-04-29 18:01:55 +0100

    created edit and studentList pages. addition of methods to edit and delete users

--- src/main/java/com/integratedAssessment/cct/controllers/StudentController.java
+++ src/main/java/com/integratedAssessment/cct/controllers/StudentController.java
@@ -1,6 +1,7 @@
 package com.integratedAssessment.cct.controllers;
 
 
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -31,29 +33,54 @@ public class StudentController {
 		mv.addObject("student", new Student());
 		return mv;
 	}
-	
 
-	
+
 	@PostMapping("/registerStudent")
-	public ResponseEntity<String> registerStudent(@ModelAttribute Student student, Model model) {
+	public String registerStudent(@ModelAttribute Student student, Model model) {
 	    String id = UUID.randomUUID().toString(); // create a unique String ID
 	    student.setId(id);
 	    Student savedStudent = studentRepository.save(student);
-	    return ResponseEntity.ok("Student saved with ID " + savedStudent.getId());
+	    return "redirect:/registered-students";
 	}
+	
+
+	@GetMapping("registered-students")
+	public ModelAndView listingStudents() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("Student/studentList");
+		mv.addObject("studentList",studentRepository.findAll());
+		return mv;
+	}
+	
+	
+	@GetMapping("/edit/{id}")
+	public ModelAndView edit(@PathVariable("id") String id) {
+	    ModelAndView mv = new ModelAndView();
+	    mv.setViewName("Student/edit");
+	    Optional<Student> student = studentRepository.findById(id);
+	    if (student.isPresent()) {
+	        mv.addObject("student", student.get());
+	    } else {
+	        // Handle the case when the student is not found by ID
+	    }
+	    return mv;
+	}
+	
+	@PostMapping("/edit")
+	public ModelAndView edit(Student student) {
+		ModelAndView mv = new ModelAndView();
+		studentRepository.save(student);
+		mv.setViewName("redirect:/registered-students");
+		return mv;
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteStudent(@PathVariable("id") String id) {
+		studentRepository.deleteById(id);
+		return "redirect:/registered-students";
+	}
+	
+
 }
 	
-//@GetMapping("/students/{id}")
-//public ResponseEntity<Student> getStudentById(@PathVariable String id) {
-//    Optional<Student> optionalStudent = studentRepository.findById(id); // buscar o Student com o ID fornecido
-//    if (optionalStudent.isPresent()) {
-//        Student student = optionalStudent.get();
-//        return ResponseEntity.ok(student); // retornar o Student encontrado
-//    } else {
-//        return ResponseEntity.notFound().build(); // retornar um erro 404 se não for encontrado nenhum Student com o ID fornecido
-//    }
-//}
-//
-//
-
 

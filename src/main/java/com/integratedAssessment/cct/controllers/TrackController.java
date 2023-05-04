f2f782325162858d1ab00016c94296f55c637f28
Bekezhan <72031152+Bekardi@users.noreply.github.com>
2023-05-04 23:20:18 +0100

    Added Update and Delete Functionality
    Created methods :
    getOneSong() -gets one song by ObjectID and returns a new html page with this document/object that allows us to edit the file and return it to the db
    deleteTrack() - allows us to delete any 1 track and returns the user to the same page with the same page and sorting configurations

--- src/main/java/com/integratedAssessment/cct/controllers/TrackController.java
+++ src/main/java/com/integratedAssessment/cct/controllers/TrackController.java
@@ -1,6 +1,7 @@
 package com.integratedAssessment.cct.controllers;
 
 import com.integratedAssessment.cct.model.Track;
+import com.integratedAssessment.cct.model.User;
 import com.integratedAssessment.cct.services.TrackService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tracks")
@@ -17,12 +19,13 @@ public class TrackController {
     @Autowired
     private TrackService trackService;
 
-
+    //returns hot100 main trackList page
+    //READ & SORT
     @GetMapping()
     public ModelAndView getTrackView(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(required = false, defaultValue = "popularity") String sortField) {
-        ModelAndView mav = new ModelAndView("chart/tracksView");
+        ModelAndView mav = new ModelAndView("musicChart/tracksView");
         int pageSize = 100;
         int offset = page * pageSize;
         List<Track> tracks = trackService.getSortedTrackListBy_Param(offset, pageSize, sortField, Sort.Direction.DESC);
@@ -34,12 +37,46 @@ public class TrackController {
         return mav;
     }
 
-    @GetMapping("/{id}")
-    public ModelAndView getOneSong(@PathVariable ObjectId id) {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("tracksView");
-        mav.addObject("track", trackService.findOne(id));
+//    @GetMapping("/edit/{id}")
+//    public ModelAndView getOneSong(@PathVariable ObjectId id) {
+//        ModelAndView mav = new ModelAndView("musicChart/editTrack");
+//        mav.addObject("track", trackService.findOne(id));
+//        return mav;
+//    }
+    //UPDATE
+    @GetMapping("/edit/{id}")
+    public ModelAndView getOneSong(
+            @PathVariable ObjectId id) {
+        ModelAndView mav = new ModelAndView("musicChart/editTrack");
+        Track foundTrack = trackService.findOne(id);
+        if (foundTrack != null) {
+            mav.addObject("track", foundTrack);
+        } else {
+            System.out.println("Error !!!");
+        }
         return mav;
     }
-
+    //DELETE
+//    @GetMapping("/delete/{id}")
+//    public String deleteTrack(
+//            @PathVariable ObjectId id,
+//            @RequestParam(name = "page") int page,
+//            @RequestParam(name = "sortField") String sortField) {
+//        trackService.deleteTrack(id);
+//        return "redirect:/tracks?page=" + page + "&sortField" + sortField;
+//    }
+    @PostMapping ("/{id}")
+    public ModelAndView deleteTrack(
+            @PathVariable ObjectId id,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "track_name") String sortField) {
+        //trackService.deleteTrack(id);
+        System.out.println("deleted track with id " + id + "\n" +
+                "page " + page + " sort " + sortField);
+        ModelAndView mav = new ModelAndView("redirect:/tracks");
+        mav.addObject("page", page);
+        mav.addObject("sortField", sortField);
+        return mav;
+       // return "redirect:/tracks?page=" + page + "&sortField=" + sortField;
+    }
 }
--- src/main/java/com/integratedAssessment/cct/services/TrackService.java
+++ src/main/java/com/integratedAssessment/cct/services/TrackService.java
@@ -17,21 +17,16 @@ public class TrackService {
     // (autowired instantiates the class for us, so we don't need initialize this instance)
     @Autowired
     private TrackRepository trackRepository;
-
-    //db access method
-    public List<Track> allTracks() {
-        //findAll() described in the Mongo Repository Class and it returns a list of the datatype that we
-        //defined in the TrackRepository class
-        return trackRepository.findAll();
-    }
     // returns List<Track> sorted by the one of the field that we pass as a parameter
     public List<Track> getSortedTrackListBy_Param(int offset, int limit, String field, Sort.Direction direction) {
         return trackRepository.findAll(PageRequest.of(offset, limit, Sort.by(direction, field))).getContent();
     }
-
-
     public Track findOne(ObjectId id) {
         Optional<Track> foundTrack = trackRepository.findById(id);
         return foundTrack.orElse(null);
+    }
+
+    public void deleteTrack(ObjectId id) {
+        trackRepository.deleteById(id);
     }
 }
